811d88f6eae8e4012c495a2e1aad978beb221d22
Franco <fegll87@gmail.com>
2022-02-14 00:21:51 -0300

    db second commit

--- api/src/routes/Pokemons.js
+++ api/src/routes/Pokemons.js
@@ -1,29 +1,32 @@
 const { Router } = require('express')
 const fetch = require("node-fetch");
-const { Pokemon } = require('../db.js')
+const { Pokemon, Tipo } = require('../db.js')
 
 const router = Router();
 
-let randomOffset = Math.random().toString()
-
-
 const getPokemonsAPI = async () => {
-  const api = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${randomOffset}&limit=40`);
+  const api = await fetch("https://pokeapi.co/api/v2/pokemon");
   const poke = await api.json()
+  var url2 = poke.next
+  const api2 = await fetch(url2)
+  const poke2 = await api2.json()
+  
+  const pokesResult = [...poke.results, ...poke2.results]
 
-  let pokeResults = poke.results
   let arrPropsPoke = []
 
-  if (poke.results.length) {
-    for (let i = 0; i < pokeResults.length; i++) {
-      const apiResUrl = await fetch(`${pokeResults[i].url}`)
+  if (pokesResult.length) {
+    for (let i = 0; i < pokesResult.length; i++) {
+      const apiResUrl = await fetch(`${pokesResult[i].url}`)
       const apiDetailsPoke = await apiResUrl.json()
+
       const detailsPoke = {
-        id: pokeResults[i].url.split("/")[6] + "_api",
+        id: pokesResult[i].url.split("/")[6] + "_api",
         name: apiDetailsPoke.name,
         image: apiDetailsPoke.sprites.other["official-artwork"].front_default || apiDetailsPoke.sprites.other.home["front_shiny"],
         types: apiDetailsPoke.types.map(el => el.type.name)
       }
+
       arrPropsPoke.push(detailsPoke)
     }
   }
@@ -31,27 +34,95 @@ const getPokemonsAPI = async () => {
   return arrPropsPoke;
 }
 
-const getPokemonsDB = () => {
-  
+const getPokemonsDB = async () => {
+  return await Pokemon.findAll({
+    include: {
+      model: Tipo,
+      attributes: ["nombre"],
+      through: {
+        attributes: []
+      }
+    }
+})
+}
+
+const getAllPokemons = async () => {
+  const infoAPI = await getPokemonsAPI();
+  const infoDB = await getPokemonsDB();
+  const infoTotalPokes = [...infoAPI, ...infoDB]
+
+  return infoTotalPokes;
 }
 
 router.get("/", async (req, res) => {
-  res.json(await getPokemonsAPI())
+  res.json(await getAllPokemons())
+})
+
+router.get("/:id", async (req, res) => {
+  const {id} = req.params
+  if (!id.includes("_api")) {
+    const pokeDB = await getPokemonsDB()
+    const findPokeDB = pokeDB.find(el => el.id === id)
+    
+    if (findPokeDB) {
+      return res.status(200).json(findPokeDB)
+    }
+    res.status(404).send("El pokemon no existe")
+  }
+  
+  let idApi = id.split("_")[0]
+  console.log(idApi);
+  if (id.includes("_api")){
+    const api = await fetch(`https://pokeapi.co/api/v2/pokemon/${idApi}`);
+    const pokeAPI = await api.json()
+    const pokeDetailsAPI = {
+      hp: pokeAPI.stats[0].base_stat,
+      attack: pokeAPI.stats[1].base_stat,
+      defense: pokeAPI.stats[2].base_stat,
+      speed: pokeAPI.stats[5].base_stat,
+      height: pokeAPI.height,
+      weight: pokeAPI.weight,
+    }
+
+    const pokeArray = await getPokemonsAPI()
+    const findPokedetailsAPI = pokeArray.find(el => el.id === id)
+
+    if (findPokedetailsAPI) {
+      const details = {...findPokedetailsAPI, ...pokeDetailsAPI}
+      return res.status(200).json(details)
+    }
+    res.status(404).send("El pokemon no existe")
+  }
 })
 
 router.post("/", async (req, res) => {
   const {name, hp, attack, defense, speed, height, weight, image, types} = req.body;
 
   const [newPoke, created] = await Pokemon.findOrCreate({
-    where: {name, hp, attack, defense, speed, height, weight, image},
-  
+    where: {
+      name, 
+      hp, 
+      attack, 
+      defense, 
+      speed, 
+      height, 
+      weight, 
+      image
+    },
   })
 
   if (!created) {
     return res.status(404).send("El pokemon ya existe")
   }
 
-  res.status(201).json(newPoke)
+  let tipoPoke = await Tipo.findAll({
+    where: {nombre: types},
+  })
+
+  await newPoke.addTipos(tipoPoke)
+  //await newPoke.setTipos(types)
+
+  res.send("Pokemon creado")
 
 })
 
