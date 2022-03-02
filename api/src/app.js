5fafc0c5a308de15790bed0b4cff613bead79fa2
Franco <fegll87@gmail.com>
2022-03-01 21:57:35 -0300

    removing changes backend

--- api/src/app.js
+++ api/src/app.js
@@ -21,9 +21,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-server.get("/", (req, res) => {
-  res.json({ info: "Pagina cargada cpn exito" });
-});
+
 server.use('/', routes);
 
 // Error catching endware.
