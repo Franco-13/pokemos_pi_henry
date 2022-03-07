2b81aecd421546b1382812b069b06a302791f2ca
Franco <fegll87@gmail.com>
2022-03-07 11:29:01 -0300

    Refactor styles LandingPage

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,35 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { BtnContainer, ImageLoading, LandingContainer, Title } from './styles';
+import { BtnAndLoadingContainer, BtnContainer, ImageLoading, LandingContainer, Title } from './styles';
 import { GlobalButton } from './../../components/GlobalButton/GlobalButton';
 import { YELLOW_PIKACHU } from '../../styles/global';
-
 
 export const LandingPage = () => {
   const allPokemons = useSelector((state) => state.pokemons)
 
   return (
     <LandingContainer>
-      <Title>
-        <h1>BIENVENIDOS</h1>
-      </Title>
-        
-        <ImageLoading isVisible={allPokemons.length} >
-          <img src="https://i.imgur.com/XLJxE8S.gif" alt="loading" />
-        </ImageLoading>
-        {allPokemons.length &&
-          <BtnContainer >
-          <Link to="/home">
-              <GlobalButton
+        <Title>
+          <h1>BIENVENIDOS</h1>
+        </Title>
+        <BtnAndLoadingContainer>
+          <ImageLoading isVisible={allPokemons.length} >
+            <img src="https://i.imgur.com/XLJxE8S.gif" alt="loading" />
+          </ImageLoading>
+          {
+            allPokemons.length &&
+            <BtnContainer >
+              <Link to="/home">
+                <GlobalButton
                   textBtn="INGRESAR"
                   fontColor="black"
                   colorBtn={YELLOW_PIKACHU}
-                  fontSize="1rem"
                 />
-            </Link>
-        </BtnContainer>}
-      
+              </Link>
+            </BtnContainer>
+          }
+        </BtnAndLoadingContainer>
     </LandingContainer>
   )
 }
--- client/src/pages/LandingPage/styles.js
+++ client/src/pages/LandingPage/styles.js
@@ -1,18 +1,24 @@
 import styled from "styled-components";
+import { MOVILE_L, MOVILE_S, TABLET } from "../../styles/global";
 
 export const LandingContainer = styled.div`
-  background-image: url("https://wallpapercave.com/wp/wp2566722.jpg");
-  background-size: cover;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  `
+  justify-content: space-evenly;
+  height: 100vh;
+  background-image: url("https://wallpapercave.com/wp/wp2566722.jpg");
+  background-size: cover;
+  background-position: center;
+  @media only screen and (max-width:${TABLET}){
+    background-position: center;
+    background-color: black;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+`
 export const Title = styled.div`
-  margin: .5rem 1rem;
-  position: absolute;
-  top: 4rem;
+  height: 25vh;
   h1{
     font-size: 5rem;
     box-shadow: inset 0px 0px 7px 8px;
@@ -20,15 +26,34 @@ export const Title = styled.div`
     padding: 2rem;
     border-radius: .5rem;
   } 
+  @media only screen and (max-width:${TABLET}){
+    h1{
+      font-size: 3rem;
+    }
+  }
+  @media only screen and (max-width:${MOVILE_L}){
+    h1{
+      font-size: 2rem;
+    }
+  }
+  @media only screen and (max-width:${MOVILE_S}){
+    h1{
+      font-size: 1.5rem;
+    }
+  }
+`
+export const BtnAndLoadingContainer = styled.div`
+  height: 25vh;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 export const BtnContainer = styled.div`
   position: absolute;
-  top: 25rem;
   animation-name: visiblility1;
-  //animation-delay: 1s;
   animation-duration: 4s;
   animation-fill-mode: forwards;
-  //z-index:100;
   @keyframes visiblility1 {
     0%{
       opacity: 0;
@@ -39,19 +64,22 @@ export const BtnContainer = styled.div`
   }
   button{
     cursor: pointer;
+    font-size: 1rem;
   }
-  img{
-    height: 9.2rem;
-    box-shadow: inset 0px 0px 7px 8px;
-    background-color: #64a8e2b8;
-    padding: 1rem 1rem;
-    border-radius: 50%;
+  @media only screen and (max-width:${TABLET}){
+    button{
+      font-size: .75rem;
+    }
+  }
+  @media only screen and (max-width:${MOVILE_L}){
+    button{
+      font-size: .5rem;
+    }
   }
 }
 `
 export const ImageLoading = styled.div`
   position: absolute;
-  top: 21rem;
   animation-name: visiblility;
   animation-duration: 4s;
   animation-fill-mode: forwards;
@@ -64,11 +92,21 @@ export const ImageLoading = styled.div`
     }
   }
   img{
-    height: 9.2rem;
+    height: 8rem;
     box-shadow: inset 0px 0px 7px 8px;
     background-color: #64a8e2b8;
     padding: 1rem 1rem;
     border-radius: 50%;
+  }
+  @media only screen and (max-width:${TABLET}){
+    img{
+      height: 6rem;
+    }
+  }
+  @media only screen and (max-width:${MOVILE_L}){
+    img{
+      height: 4rem;
+    }
   }
 }
 `
