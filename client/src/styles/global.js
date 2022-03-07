908810dffb2cbbbd08e1e8222549fce75e5aa3e3
Franco <fegll87@gmail.com>
2022-03-07 11:25:10 -0300

    Update global.js
    add constants for responsive design

--- client/src/styles/global.js
+++ client/src/styles/global.js
@@ -8,3 +8,8 @@ export const COLOR_RED_TRANSPARENT = "#b81613de"
 export const COLOR_RED = "#b81613"
 export const SMOOTH_WHITE_POKEBALL = "#e7e7e7"
 export const YELLOW_PIKACHU = "#f3cf1e"
+
+export const LAPTOP="1025px"
+export const TABLET = "769px";
+export const MOVILE_L = "426px";
+export const MOVILE_S = "321px";
